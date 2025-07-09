3fd0229d5ace4791a36726a197da90896b921323
juansubas <juansubas@gmail.com>
2025-07-09 10:15:07 -0500

    Fixed Variable PORT HAHAHA

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { serve } from '@hono/node-server'
 import 'dotenv/config'
 import app from './app'
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3000 ;
+const PORT = Number(process.env.PORT) || 3000
 
-serve({ fetch: app.fetch, port: 3000 }, (info) => {
+serve({ fetch: app.fetch, port: PORT }, (info) => {
   console.log(`🚀 Server running at http://localhost:${info.port}`)
 })
