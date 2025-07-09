b5f61329d9a9f176f1c5845c6963d74b1a9bf3c5
juansubas <juansubas@gmail.com>
2025-07-09 10:11:58 -0500

    PORT .ENV

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { serve } from '@hono/node-server'
 import 'dotenv/config'
 import app from './app'
 
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000 ;
+
 serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`🚀 Server running at http://localhost:${info.port}`)
 })
